0bc1b4dfca08cf4013a595a61c8524d8c13b42c7
Zaid <zaid.kpj@gmail.com>
2021-12-24 02:23:36 +0530

    Selection Sort

--- src/lecture/Arrays/Sorting.java
+++ src/lecture/Arrays/Sorting.java
@@ -12,36 +12,93 @@ public class Sorting {
 //        System.out.println(Arrays.toString(new int[][]{{1},{23},{4}}));
 
         int[] sortedArray = bubbleSort(num);
+//        int[] sortedArray = selectionSort(num);
 
-        System.out.println(Arrays.toString(num));
+        System.out.println(Arrays.toString(sortedArray));
 
 
     }
 
-    static int[] bubbleSort(int[] num) {
-
-        boolean swapped;
+    static int[] selectionSort(int[] num) {
 
         for (int i = 0; i < num.length; i++) {
-            swapped = false;
-            for (int j = 1; j < num.length - i; j++) {
 
-                if(num[j] < num[j - 1]){
+            int last = num.length - i - 1;
+            int maxIndex = findMax(num, 0, last);
+            swap(num,maxIndex,last);
 
-                    int temp = num[j];
-                    num[j] = num[j - 1];
-                    num[j - 1] = temp;
-                    swapped = true;
-                }
-
-            }
-            if(!swapped) break;
 
         }
 
         return num;
 
     }
+
+    static void swap(int[] num, int first, int second) {
+
+        int temp = num[first];
+        num[first] = num[second];
+        num[second] = temp;
+
+
+    }
+
+    private static int findMax(int[] num,int start,int last) {
+
+        int maxIndex = start;
+
+        for (int i = 1; i <= last; i++) {
+
+            if(num[maxIndex] < num[i]){
+                maxIndex = i;
+            }
+
+        }
+
+        return maxIndex;
+
+    }
+
+    static int[] bubbleSort(int[] num){
+
+        for (int i = 0; i < num.length; i++) {
+            boolean swapped = false;
+            for (int j = 1; j < num.length - i; j++) {
+                if(num[j] < num[j - 1]){
+                    swap(num,j,j-1);
+                    swapped = true;
+                }
+            }
+            if(!swapped) return num;
+        }
+
+        return num;
+    }
+
+//    static int[] bubbleSort(int[] num) {
+//
+//        boolean swapped;
+//
+//        for (int i = 0; i < num.length; i++) {
+//            swapped = false;
+//            for (int j = 1; j < num.length - i; j++) {
+//
+//                if(num[j] < num[j - 1]){
+//
+//                    int temp = num[j];
+//                    num[j] = num[j - 1];
+//                    num[j - 1] = temp;
+//                    swapped = true;
+//                }
+//
+//            }
+//            if(!swapped) break;
+//
+//        }
+//
+//        return num;
+//
+//    }
 
 
 }
